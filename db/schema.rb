9a211f95a6c55fc49c6d04a0a415f1d39a61abb0
kyle smith <chessmonger2112@yahoo.com>
2016-06-04 16:26:46 -0700

    changed the gemfile lock to update rake version

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160303031821) do
+ActiveRecord::Schema.define(version: 20160303031639) do
 
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "message_id"
-    t.integer  "user_id"
   end
 
   create_table "messages", force: :cascade do |t|
